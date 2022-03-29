e6c0624b2d815c16e0d6c2e59f46dfc24d1666cd
Mateo Vial <vialmateovial@gmail.com>
2022-03-30 00:16:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from classconnect4 import *
 
-Game = Connect4()
-
-while True:
+if __name__ == '__main__':
     
-    Game.affiche()
-    if Game.tour:
-        print('Au tour des jaunes')
-    else:
-        print('Au tour des rouges')
-    actionchar = input('Action [0-6 = jouer, s = stop] : ')
+    Game = Connect4()
 
-    if actionchar in ['0', '1', '2', '3', '4', '5', '6']:
-        Game.jouer(int(actionchar))
-    elif actionchar == 's':
-        break
-    else:
-        print('Action inconnue')
+    while True:
+    
+        Game.affiche()
+        if Game.tour:
+            print('Au tour des jaunes')
+        else:
+            print('Au tour des rouges')
+        actionchar = input('Action [0-6 = jouer, s = stop] : ')
+
+        if actionchar in ['0', '1', '2', '3', '4', '5', '6']:
+            Game.jouer(int(actionchar))
+        elif actionchar == 's':
+            break
+        else:
+            print('Action inconnue')
